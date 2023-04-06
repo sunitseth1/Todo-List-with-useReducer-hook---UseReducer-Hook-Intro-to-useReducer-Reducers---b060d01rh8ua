d3716a173ff662c2cc77f50caabf172f21552a15
sunit seth <100702483+sunitseth1@users.noreply.github.com>
2023-04-06 23:46:03 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,14 @@
-import React from 'react';
-const Todo = () => {
-    return (
-        <></>
-    )
-}
+import React from "react";
+import "../styles/App.css";
+const Todo = ({ title, id, handleDelete }) => {
+  return (
+    <div className="todo">
+      <div className="todo-title">{title}</div>
+      <button className="todo-delete" onClick={() => handleDelete(id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
 
-
-export { Todo }
-
+export { Todo };
