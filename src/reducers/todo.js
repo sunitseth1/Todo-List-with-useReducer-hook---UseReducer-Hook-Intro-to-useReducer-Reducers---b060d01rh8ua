4bba4bb27c825f8278aa5dc28e7bd1b3986a9e75
sunit seth <100702483+sunitseth1@users.noreply.github.com>
2023-04-06 23:51:53 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,7 +1,28 @@
 const todoReducer = (state, action) => {
-    switch (action.type) {
-
+  switch (action.type) {
+    case "ADD": {
+      return {
+        todos: [
+          ...state.todos,
+          {
+            title: action.value,
+            id:
+              new Date().toLocaleDateString() +
+              "" +
+              new Date().toLocaleTimeString(),
+          },
+        ],
+      };
     }
-}
+    case "DELETE": {
+      return {
+        todos: action.data,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
-export { todoReducer }
+export { todoReducer };
